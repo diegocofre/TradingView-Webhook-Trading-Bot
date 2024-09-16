b2ed8078d80e3287dbd8d3a62baa6a3bfef27723
Diego Cofré <diego.cofre@gmail.com>
2024-09-16 09:17:47 -0300

    Bybit V5

--- bybitapi.py
+++ bybitapi.py
@@ -1,10 +1,13 @@
 import logbot
-from pybit import HTTP
+from pybit.unified_trading import HTTP
+import json
+import os
+from flask import Flask, request
+
+app = Flask(__name__)
 
 class ByBit:
     def __init__(self, var: dict):
-        self.ENDPOINT = 'https://api-testnet.bybit.com'
-
         self.subaccount_name = var['subaccount_name']
         self.leverage = var['leverage']
         self.risk = var['risk']
@@ -14,55 +17,48 @@ class ByBit:
     # =============== SIGN, POST AND REQUEST ===============
 
     def _try_request(self, method: str, **kwargs):
-        session = HTTP(self.ENDPOINT, api_key=self.api_key, api_secret=self.api_secret)
+        session = HTTP(api_key=self.api_key, api_secret=self.api_secret)
         try:
-            if method=='get_wallet_balance':
-                req = session.get_wallet_balance(coin=kwargs.get('coin'))
-            elif method=='my_position':
-                req = session.my_position(symbol=kwargs.get('symbol'))
-            elif method=='place_active_order':
-                req = session.place_active_order(symbol=kwargs.get('symbol'), 
-                                                    side=kwargs.get('side'), 
-                                                    order_type=kwargs.get('order_type'), 
-                                                    qty=kwargs.get('qty'), 
-                                                    price=kwargs.get('price', None), 
-                                                    stop_loss=kwargs.get('stop_loss', None), 
-                                                    time_in_force=kwargs.get('time_in_force'), 
-                                                    reduce_only=kwargs.get('reduce_only'), 
-                                                    close_on_trigger=kwargs.get('close_on_trigger'))
-            elif method=='place_conditional_order':
-                req = session.place_conditional_order(symbol=kwargs.get('symbol'),
-                                                        side=kwargs.get('side'),
-                                                        order_type=kwargs.get('order_type'),
-                                                        qty=kwargs.get('qty'),
-                                                        price=kwargs.get('price'),
-                                                        base_price=kwargs.get('base_price'),
-                                                        stop_px=kwargs.get('stop_px'),
-                                                        trigger_by=kwargs.get('trigger_by'),
-                                                        time_in_force=kwargs.get('time_in_force'),
-                                                        reduce_only=kwargs.get('reduce_only'),
-                                                        close_on_trigger=kwargs.get('close_on_trigger'))
-            elif method=='cancel_all_active_orders':
-                req = session.cancel_all_active_orders(symbol=kwargs.get('symbol'))
-            elif method=='cancel_all_conditional_orders':
-                req = session.cancel_all_conditional_orders(symbol=kwargs.get('symbol'))
-            elif method=='set_trading_stop':
-                req = session.set_trading_stop(symbol=kwargs.get('symbol'), 
-                                                side=kwargs.get('side'), # Side of the open position
-                                                stop_loss=kwargs.get('stop_loss'))
-            elif method=='query_symbol':
-                req = session.query_symbol()
+            if method == 'get_wallet_balance':
+                req = session.get_wallet_balance(accountType="UNIFIED", coin=kwargs.get('coin'))
+            elif method == 'my_position':
+                req = session.get_positions(category="linear", symbol=kwargs.get('symbol'))
+            elif method == 'place_active_order':
+                req = session.place_order(category="linear", symbol=kwargs.get('symbol'), 
+                                          side=kwargs.get('side'), orderType=kwargs.get('order_type'), 
+                                          qty=kwargs.get('qty'), price=kwargs.get('price', None), 
+                                          stopLoss=kwargs.get('stop_loss', None), 
+                                          timeInForce=kwargs.get('time_in_force'), 
+                                          reduceOnly=kwargs.get('reduce_only'), 
+                                          closeOnTrigger=kwargs.get('close_on_trigger'))
+            elif method == 'place_conditional_order':
+                req = session.place_conditional_order(category="linear", symbol=kwargs.get('symbol'),
+                                                      side=kwargs.get('side'), order_type=kwargs.get('order_type'),
+                                                      qty=kwargs.get('qty'), price=kwargs.get('price'),
+                                                      basePrice=kwargs.get('base_price'), 
+                                                      trigger_price=kwargs.get('stop_px'),
+                                                      triggerBy=kwargs.get('trigger_by'), 
+                                                      timeInForce=kwargs.get('time_in_force'),
+                                                      reduceOnly=kwargs.get('reduce_only'), 
+                                                      closeOnTrigger=kwargs.get('close_on_trigger'))
+            elif method == 'cancel_all_active_orders':
+                req = session.cancel_all_orders(category="linear", symbol=kwargs.get('symbol'))
+            elif method == 'set_trading_stop':
+                req = session.set_trading_stop(category="linear", symbol=kwargs.get('symbol'), 
+                                               side=kwargs.get('side'), stopLoss=kwargs.get('stop_loss'))
+            elif method == 'query_symbol':
+                req = session.get_instruments_info(category="linear")
         except Exception as e:
-            logbot.logs('>>> /!\ An exception occured : {}'.format(e), True)
+            logbot.logs('>>> /!\ An exception occurred: {}'.format(e), True)
             return {
                 "success": False,
                 "error": str(e)
             }
-        if req['ret_code']:
-            logbot.logs('>>> /!\ {}'.format(req['ret_msg']), True)
+        if req['retCode']:
+            logbot.logs('>>> /!\ {}'.format(req['retMsg']), True)
             return {
                     "success": False,
-                    "error": req['ret_msg']
+                    "error": req['retMsg']
                 }
         else:
             req['success'] = True
@@ -94,38 +90,59 @@ class ByBit:
             stop_loss = payload['short SL']
             take_profit = payload['short TP']
         
-        r = self._try_request('query_symbol')
-        r = r['result']
-        my_item = next((item for item in r if item['name'] == 'BTCUSDT'), None)
-        qty_step = my_item['lot_size_filter']['qty_step']
+        r = self._try_request('query_symbol')      
+        if not r['success']:
+            return r
+        
+        r = r['result']['list']
+        my_item = next((item for item in r if item['symbol'] == 'BTCUSDT'), None)
+        if not my_item:
+            return {
+                "success": False,
+                "error": "Symbol BTCUSDT not found"
+            }
+        qty_step = my_item['lotSizeFilter']['qtyStep']
 
         # 0/ Get free collateral and calculate position
         r = self._try_request('get_wallet_balance', coin="USDT")
         if not r['success']:
             return r
-        free_collateral = r['result']['USDT']['available_balance']
-        logbot.logs('>>> Found free collateral : {}'.format(free_collateral))
+        
+        list = r['result']['list']
+        # Encontrar el diccionario que tiene 'coin' igual a 'USDT'
+        usdt_info = next((item for account in list for item in account['coin'] if item['coin'] == 'USDT'), None)
+
+        if usdt_info:
+           free_collateral = float(usdt_info['walletBalance'])
+           
+        if not free_collateral: 
+            return {
+                "success": False,
+                "error": "USDT balance not found"
+            }
+
+        logbot.logs('>>> Found free collateral: {}'.format(free_collateral))
         size = (free_collateral * self.risk) / abs(payload['price'] - stop_loss)
         if (size / (free_collateral / payload['price'])) > self.leverage:
             return {
-                    "success" : False,
-                    "error" : "leverage is higher than maximum limit you set"
+                    "success": False,
+                    "error": "leverage is higher than maximum limit you set"
                 }
         
         size = self._rounded_size(size, qty_step)
 
-        logbot.logs(f">>> SIZE : {size}, SIDE : {side}, PRICE : {payload['price']}, SL : {stop_loss}, TP : {take_profit}")
+        logbot.logs(f">>> SIZE: {size}, SIDE: {side}, PRICE: {payload['price']}, SL: {stop_loss}, TP: {take_profit}")
      
         # 1/ place order with stop loss
         if 'type' in payload.keys():
-            order_type = payload['type'] # 'market' or 'limit'
+            order_type = payload['type']  # 'market' or 'limit'
             order_type = order_type.capitalize()
         else:
-            order_type = 'Market' # per defaut market if none is specified
+            order_type = 'Market'  # per default market if none is specified
         if order_type != 'Market' and order_type != 'Limit':
             return {
-                    "success" : False,
-                    "error" : f"order type '{order_type}' is unknown"
+                    "success": False,
+                    "error": f"order type '{order_type}' is unknown"
                 }
         exe_price = None if order_type == "Market" else payload['price']
         r = self._try_request('place_active_order', 
@@ -150,7 +167,7 @@ class ByBit:
                 r = self._try_request('place_active_order', 
                                     symbol=ticker, 
                                     side=close_sl_tp_side, 
-                                    order_type="Limit", # so we avoid paying fees on market take profit
+                                    order_type="Limit",  # so we avoid paying fees on market take profit
                                     qty=size, 
                                     price=take_profit,
                                     time_in_force="GoodTillCancel", 
@@ -161,7 +178,7 @@ class ByBit:
                     return r
                 orders.append(r['result'])
                 logbot.logs(">>> Take profit posted with success")
-            else: # Limit order type
+            else:  # Limit order type
                 r = self._try_request('place_conditional_order', 
                                     symbol=ticker, 
                                     side=close_sl_tp_side, 
@@ -172,7 +189,7 @@ class ByBit:
                                     stop_px=exe_price, 
                                     trigger_by='LastPrice', 
                                     time_in_force="GoodTillCancel", 
-                                    reduce_only=False, # Do not set to True
+                                    reduce_only=False,  # Do not set to True
                                     close_on_trigger=False)
                 if not r['success']:
                     r['orders'] = orders
@@ -183,18 +200,18 @@ class ByBit:
         # 3/ (optional) place multiples take profits
         i = 1
         while True:
-            tp = 'tp' + str(i) + ' Mult'
+            tp = f'tp{i}'
             if tp in payload.keys():
                 # place limit order
                 dist = abs(payload['price'] - stop_loss) * payload[tp]
-                mid_take_profit = (payload['price'] + dist) if  side == 'Buy' else (payload['price'] - dist)
+                mid_take_profit = (payload['price'] + dist) if side == 'Buy' else (payload['price'] - dist)
                 mid_size = size * (payload['tp Close'] / 100)
                 mid_size = self._rounded_size(mid_size, qty_step)
                 if order_type == 'Market':
                     r = self._try_request('place_active_order', 
                             symbol=ticker, 
                             side=close_sl_tp_side, 
-                            order_type="Limit", # so we avoid paying fees on market take profit
+                            order_type="Limit",  # so we avoid paying fees on market take profit
                             qty=mid_size, 
                             price=mid_take_profit,
                             time_in_force="GoodTillCancel", 
@@ -205,7 +222,7 @@ class ByBit:
                         return r
                     orders.append(r['result'])
                     logbot.logs(f">>> Take profit {i} posted with success at price {mid_take_profit} with size {mid_size}")
-                else: # Stop limit type
+                else:  # Stop limit type
                     r = self._try_request('place_conditional_order', 
                                     symbol=ticker, 
                                     side=close_sl_tp_side, 
@@ -216,7 +233,7 @@ class ByBit:
                                     stop_px=exe_price, 
                                     trigger_by='LastPrice', 
                                     time_in_force="GoodTillCancel", 
-                                    reduce_only=False, # Do not set to True
+                                    reduce_only=False,  # Do not set to True
                                     close_on_trigger=False)
                     if not r['success']:
                         r['orders'] = orders
@@ -232,7 +249,6 @@ class ByBit:
             "orders": orders
         }
 
-
     def exit_position(self, ticker):
         #   CLOSE POSITION IF ONE IS ONGOING
         r = self._try_request('my_position', symbol=ticker)
@@ -240,8 +256,8 @@ class ByBit:
             return r
         logbot.logs(">>> Retrieve positions")
 
-        for position in r['result']:
-            open_size = position['size']
+        for position in r['result']['list']:
+            open_size = float(position['size'])
             if open_size > 0:
                 open_side = position['side']
                 close_side = 'Sell' if open_side == 'Buy' else 'Buy'
@@ -262,19 +278,16 @@ class ByBit:
 
                 break
 
-        #   DELETE ALL OPEN AND CONDITIONAL ORDERS REMAINING
+        #   DELETE ALL ORDERS REMAINING
         r = self._try_request('cancel_all_active_orders', symbol=ticker)
         if not r['success']:
             return r
-        r = self._try_request('cancel_all_conditional_orders', symbol=ticker)
-        if not r['success']:
-            return r
-        logbot.logs(">>> Deleted all open and conditional orders remaining with success")
+
+        logbot.logs(">>> Deleted all orders remaining with success")
         
         return {
             "success": True
         }
-
 
     def breakeven(self, payload: dict, ticker):
         #   SET STOP LOSS TO BREAKEVEN
@@ -289,13 +302,12 @@ class ByBit:
             open_size = position['size']
             if open_size > 0:
                 open_side = position['side']
-                # close_side = 'Sell' if open_side == 'Buy' else 'Buy'
                 breakeven_price = payload['long Breakeven'] if open_side == 'Buy' else payload['short Breakeven']
 
                 # place market stop loss at breakeven
                 r = self._try_request('set_trading_stop', 
                                     symbol=ticker, 
-                                    side=open_side, # Side of the open position
+                                    side=open_side,  # Side of the open position
                                     stop_loss=breakeven_price)
                 if not r['success']:
                     return r
@@ -306,3 +318,29 @@ class ByBit:
             "success": True,
             "orders": orders
         }
+
+@app.route("/tradingview-to-discord-study", methods=['POST'])
+def discord_study_tv():
+    logbot.logs("========== STUDY ==========")
+    
+    data = json.loads(request.data)
+
+    webhook_passphrase = os.environ.get('WEBHOOK_PASSPHRASE', config.WEBHOOK_PASSPHRASE)
+
+    if 'passphrase' not in data.keys():
+        logbot.logs(">>> /!\ No passphrase entered", True)
+        return {
+            "success": False,
+            "message": "no passphrase entered"
+        }
+
+    if data['passphrase'] != webhook_passphrase:
+        logbot.logs(">>> /!\ Invalid passphrase", True)
+        return {
+            "success": False,
+            "message": "invalid passphrase"
+        }
+
+    orders = order(data)
+    print(orders)
+    return orders
--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 WEBHOOK_PASSPHRASE = 'setyourpassphrasehere'
 
 DISCORD_LOGS_URL = None
@@ -6,15 +8,29 @@ DISCORD_AVATAR_URL = None
 DISCORD_STUDY_URL = None
 DISCORD_STUDY_AVATAR_URL = None
 
-LEVERAGE_TESTING = None
-RISK_TESTING = None
-API_KEY_TESTING = None
-API_SECRET_TESTING = None
+LEVERAGE_TESTING = 100
+RISK_TESTING = 10
+API_KEY_TESTING = 'd76eL3NbVSUqb2zyoS'
+API_SECRET_TESTING = 'e2miW7MwIbw0ZVJ85CsrQruXAPoVtAfb2Fnq'
 
 #please review these settings with bybit api documentation
 LEVERAGE_MYBYBITACCOUNT = 50
 RISK_MYBYBITACCOUNT = 5
 
-#Testnet API Key and Secret
-API_KEY_MYBYBITACCOUNT = 'UPAFznEo72Yv5FCX1n'
-API_SECRET_MYBYBITACCOUNT = 'CTmisiLKyaoRwjp6MGOSs0n2BYHDMuC2G9Rx'
+API_KEY_MYBYBITACCOUNT = 'd76eL3NbVSUqb2zyoS'
+API_SECRET_MYBYBITACCOUNT = 'e2miW7MwIbw0ZVJ85CsrQruXAPoVtAfb2Fnq'
+
+# https://tvbot-79a793c11bdc.herokuapp.com/
+
+# git push heroku main
+# heroku restart --app tvbot
+# heroku logs --tail --app tvbot
+
+#heroku config:set LEVERAGE_TESTING=100
+#heroku config:set RISK_TESTING=your_risk_testing
+
+#heroku config:set API_KEY_TESTING ='UPAFznEo72Yv5FCX1n'
+#heroku config:set API_SECRET_TESTING='CTmisiLKyaoRwjp6MGOSs0n2BYHDMuC2G9Rx'
+
+os.environ['HTTP_PROXY'] = 'http://127.0.0.1:8888'
+os.environ['HTTPS_PROXY'] = 'http://127.0.0.1:8888'
